358b80be19ecff7d073b1a849316f6ea1655d6dd
Rahul Siwal <rahulsiwal62@gmail.com>
2022-05-01 10:27:54 +0530

    code uplaod

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# Google-Playstore-Apps-Data-Scraper
-This code scrap the data from google playstore. You can save the URL's to file and this will scrapp the data from playtore for these application urls.
+## Google Play Store Scraper PHP
+
+PHP Script to scrape application data from the Google Play store.
+
+Using this script you can download the applications following data in the CSV format.
+
+- Owner Name
+- Application Name
+- Number of total installs
+- Last Update Date and Time
+- Developer Email Id
+- Category Name
+- Rating Count
+- Application APK Size
+- Application Current Version
+- Minimum Android Version Required
+
+### Process
+
+- Get the Applications Playstore URL and save these url in the `urls.csv` file. Sample file is available [here](https://github.com/crsiwal/Google-Playstore-Apps-Data-Scraper)
+- Then run the `Scraper.php` file in command line. Syntax: `php Scraper.php`
+
+### Get Playstore Apps URL
+To get the play store apps url you can run the following `Javascript` code  
+
+```JavaScript
+/** Function for save the files data to local disk **/
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+/** Get the Package name from console using following javascript **/
+var apps = document.getElementsByClassName("poRVub");
+var apps_url = "";
+var apps_count = 0;
+for (var i = 0; i < apps.length; i++) {
+	let url = apps.item(i).href;
+	if(url.indexOf("store/apps/details") !== -1){
+		//package_name += url.split('?id=')[1] + ",";
+		apps_url += url + "\n";
+		apps_count++;
+	}
+}
+console.log(apps_count);
+download('urls.csv', apps_url);
+```
+
+- After download you should remove duplicate URL's from all apps url using `Microsoft Excel` remove duplicate tool.
+
+
+## 💻 Contributing
+
+Contributions are always welcome!
+
+See [contribution guidelines](contributing.md) for ways to get started.
+
+Please adhere to project's `code of conduct`.
+## A supporter is worth a thousand followers.
+
+If you have found anything useful and you want to support me, then Buy me a [Coffee](https://www.buymeacoffee.com/rsiwal) or [Chai](https://getmechai.vercel.app/link.html?vpa=rsiwal@upi&nm=Rahul+Siwal&amt=500)
+
+[Buy Me a Coffee](https://www.buymeacoffee.com/rsiwal) or [Buy Me a Chai](https://getmechai.vercel.app/link.html?vpa=rsiwal@upi&nm=Rahul+Siwal&amt=500) is a modern support platform by people can express appreciation for someone work. It will take a minute of you to give a appreciation to me which will encourage me to write something new for the web and it may be your next profit business.
+
+If you want to contribute feel free to do it at Buy me a [Coffee](https://www.buymeacoffee.com/rsiwal) or [Chai](https://getmechai.vercel.app/link.html?vpa=rsiwal@upi&nm=Rahul+Siwal&amt=500), and many thanks in advance.
+
+For as we well know
+
+var code = developer + caffeine
+
+😁
+
+ps. Jam doughnut are also a perfectly acceptable sign of appreciation 🍩
+
+#### User From India Can support here
+[![Support From India](https://i.ibb.co/Xkdj83y/image-2.png)](https://getmechai.vercel.app/link.html?vpa=rsiwal@upi&nm=Rahul+Siwal&amt=500)
+
+#### User From Outside India Can support here
+[![Support From Outside India](https://img.buymeacoffee.com/button-api/?text=Buy%20a%20coffee%20for%20me&emoji=&slug=rsiwal&button_colour=FF5F5F&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00)](https://www.buymeacoffee.com/rsiwal)
+
+## 🤝 Connect with me
+
+[![](https://img.shields.io/badge/Profile-Twitter-1DA1F2?style=flat&logo=twitter&color=336791&logoColor=white)](https://www.twitter.com/rahulsiwal)
+
+[![](https://img.shields.io/badge/Profile-Linkedin-0A66C2?style=flat&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/rsiwal)
+
+[![](https://img.shields.io/badge/Profile-Facebook-3b5998?style=flat&logo=facebook&logoColor=white)](https://www.facebook.com/rsiwal)
+
+[![](https://img.shields.io/badge/Profile-Instagram-3f729b?style=flat&logo=instagram&logoColor=white)](https://www.instagram.com/rahulsiwal)
+
+## Feedback
+
+If you have any feedback, please reach out to me at rsiwal@yahoo.com
+
+
+## License 
+
+[![CC0](https://licensebuttons.net/p/zero/1.0/88x31.png)](https://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, [Rahul Siwal](https://github.com/crsiwal) has waived all copyright and related or neighboring rights to this work.
---
+++ Scraper.php
@@ -0,0 +1,162 @@
+<?php
+ini_set('max_execution_time', 0);
+
+function get_app_details($url) {
+	$html = get_html($url);
+	$iserror = sub_elements($html['content'], 'error-section', true, false, true);
+	if (strpos($iserror, 'URL was not found') !== false) {
+		return false;
+	} else {
+		return get_content($url, $html['content']);
+	}
+}
+
+function get_html($url) {
+	$user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+	$options = array(
+		CURLOPT_CUSTOMREQUEST  => "GET",        //set request type post or get
+		CURLOPT_POST           => false,        //set to GET
+		CURLOPT_USERAGENT      => $user_agent, //set user agent
+		CURLOPT_COOKIEFILE     => "cookie.txt", //set cookie file
+		CURLOPT_COOKIEJAR      => "cookie.txt", //set cookie jar
+		CURLOPT_RETURNTRANSFER => true,     // return web page
+		CURLOPT_HEADER         => false,    // don't return headers
+		CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+		CURLOPT_ENCODING       => "",       // handle all encodings
+		CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+		CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+		CURLOPT_TIMEOUT        => 120,      // timeout on response
+		CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+	);
+	$ch      = curl_init($url);
+	curl_setopt_array($ch, $options);
+	$content = curl_exec($ch);
+	$err     = curl_errno($ch);
+	$errmsg  = curl_error($ch);
+	$header  = curl_getinfo($ch);
+	curl_close($ch);
+	$header['errno']   = $err;
+	$header['errmsg']  = $errmsg;
+	$header['content'] = $content;
+	return $header;
+}
+
+function get_content($url, $html) {
+	$content = array(
+		"url" => $url,
+		"appname" => trim(sub_elements($html, "AHFaub")),
+		"rating" => trim(sub_elements($html, "BHMmbe"))
+	);
+
+	$data = sub_elements($html, "R8zArc", false);
+	$content['developer'] = trim(sub_elements($data[0], "hrTbp"));
+	$content['subcategory'] = trim(sub_elements($data[1], "hrTbp"));
+	$content['ratingcount'] = str_replace(" total", "", sub_elements($html, "EymY4b"));
+	$cont = sub_elements($html, "hAyfc", false);
+	$content['updated'] = (is_array($cont) && isset($cont[0])) ? sub_elements($cont[0], "htlgb") : 'Unknown';
+	$content['size'] = (is_array($cont) && isset($cont[1])) ? sub_elements($cont[1], "htlgb") : 'Unknown';
+	$content['installs'] = (is_array($cont) && isset($cont[2])) ? sub_elements($cont[2], "htlgb") : 'Unknown';
+	$content['version'] = (is_array($cont) && isset($cont[3])) ? sub_elements($cont[3], "htlgb") : 'Unknown';
+	$content['androidneed'] = (is_array($cont) && isset($cont[4])) ? sub_elements($cont[4], "htlgb") : 'Unknown';
+	$content['contentrating'] = (is_array($cont) && isset($cont[5])) ? sub_elements($cont[5], "htlgb") : 'Unknown';
+	$content['contentrating'] = str_replace("Rated for ", "", $content['contentrating']);
+	$content['contentrating'] = str_replace("Learn More", "", $content['contentrating']);
+	$content['contactemail'] = (is_array($cont) && isset($cont[9])) ? sub_elements($cont[9], "euBY6b") : '';
+	return $content;
+}
+
+function sub_elements($html, $id, $singleNode = true, $bytagname = false, $byid = false) {
+	$identifier = ($byid == true) ? "(@id, '" . $id . "')" : "(@class, '" . $id . "')";
+	$doc = new DOMDocument();
+	libxml_use_internal_errors(true);
+	$doc->loadHTML($html);
+	$finder = new DomXPath($doc);
+	$node = $finder->query("//*[contains" . $identifier . "]");
+	if ($singleNode == true) {
+		if ($bytagname == true) {
+			$node = $doc->getElementsByTagName($id);
+			$html = $doc->saveHTML($node->item(0));
+			$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', $html);
+			return strip_tags($html);
+		} else {
+			$html = $doc->saveHTML($node->item(0));
+			$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', $html);
+			$html = preg_replace('/\s+/', ' ', $html);
+			$html = strip_tags($html);
+			return (strlen($html) > 250) ? "" : $html;
+		}
+	} else {
+		if ($bytagname == true) {
+			$array = array();
+			$list = $doc->getElementsByTagName($id);
+			foreach ($list as $row)
+				$array[] = $doc->saveHTML($row);
+			return $array;
+		} else {
+			$array = array();
+			foreach ($node as $row)
+				$array[] = $doc->saveHTML($row);
+			return $array;
+		}
+	}
+}
+
+function output_file($path, $append = true, $content = "") {
+	if ($append) {
+		file_put_contents(dirname(__FILE__) . "/" . $path, $content);
+	} else {
+		file_put_contents(dirname(__FILE__) . "/" . $path, $content, FILE_APPEND);
+	}
+}
+
+output_file('details.csv', false);
+output_file('invalid.txt', false);
+$input = fopen(dirname(__FILE__) . '/urls.csv', 'r');
+$output = fopen(dirname(__FILE__) . '/details.csv', 'a');
+fputcsv(
+	$output,
+	[
+		"Offered By",
+		"App Name",
+		"Installs",
+		"Last Update",
+		"Developer email id",
+		"Category",
+		"Rating",
+		"App Size",
+		"App Version",
+		"Min. Android Version Req.",
+		"Playstore URL"
+	]
+);
+$appcount = $invalid_url_count = 0;
+while (($line = fgetcsv($input)) !== FALSE) {
+	set_time_limit(0);
+	$content = get_app_details($line[0]);
+	if (!$content) {
+		$invalid_url_count++;
+		echo "Not available : {$line[0]}" . PHP_EOL;
+		$url_line = $line[0] . PHP_EOL;
+		output_file('invalid.txt', true, $url_line);
+	} else {
+		fputcsv($output, [
+			$content["developer"],
+			$content["appname"],
+			$content["installs"],
+			$content["updated"],
+			$content["contactemail"],
+			$content["subcategory"],
+			$content["rating"],
+			$content["size"],
+			$content["version"],
+			$content["androidneed"],
+			$content["url"],
+		]);
+		$appcount++;
+		echo "Added " . $content["appname"] . PHP_EOL;
+	}
+}
+echo "\nApps Data Found: $appcount apps\n";
+echo "\nInvalid URLS $invalid_url_count\n";
+fclose($input);
+fclose($output);
